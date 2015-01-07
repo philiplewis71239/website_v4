e5ac609fbae4a5897f1484186402370d77e65dc9
Philip Lewis <philip.lewis71239@gmail.com>
2015-01-07 11:36:30 -0500

    adding things

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
 @app.route("/")
+def gateway():
+	ip = request.remote_addr
+	return render_template("gateway.html")
+
+@app.route("/index")
 def index():
+	ip = request.remote_addr
+
 	return render_template("index.html")
 
 if __name__ == '__main__':
