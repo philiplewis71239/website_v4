fcd53c9b0b6c6c1bdd2484b26bb3034f4b793440
Philip Lewis <philip.lewis71239@gmail.com>
2015-01-07 12:29:36 -0500

    fixed it

--- app.py
+++ app.py
@@ -20,25 +20,25 @@ class Logger(db.Model):
 @app.route("/")
 def gateway():
 	ip = request.remote_addr
-	# log = Logger(ip)
-	# db.session.add(log)
-	# db.session.commit()
+	log = Logger(ip)
+	db.session.add(log)
+	db.session.commit()
 	return render_template("gateway.html")
 
 @app.route("/index")
 def index():
 	ip = request.remote_addr
-	# log = Logger(ip)
-	# db.session.add(log)
-	# db.session.commit()
+	log = Logger(ip)
+	db.session.add(log)
+	db.session.commit()
 	return render_template("index.html")
 
-# @app.route("/view_logs/<password>")
-# def view_logs(password):
-# 	if password == "temporary_password":
-# 		return render_template("viewer.html",results=Logger.query.all())
-# 	else:
-# 		return "Oh hi there"
+@app.route("/view_logs/<password>")
+def view_logs(password):
+	if password == "temporary_password":
+		return render_template("viewer.html",results=Logger.query.all())
+	else:
+		return "Oh hi there"
 
 
 if __name__ == '__main__':
