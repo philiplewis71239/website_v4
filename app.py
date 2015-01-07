7fe80e95d94e9e9e989bf8d18212f01b51dd7f4e
Philip Lewis <philip.lewis71239@gmail.com>
2015-01-07 11:59:56 -0500

    adding db

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 app = Flask(__name__)
-
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 @app.route("/")
 def gateway():
