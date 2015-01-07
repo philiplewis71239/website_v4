2c55065cc52ea28a8032c8846802f593b5d88b52
Philip Lewis <philip.lewis71239@gmail.com>
2015-01-07 12:27:27 -0500

    datetime fixed

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
+import datetime
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
