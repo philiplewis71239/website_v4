7d7225b76b6d20d638d65a56877427d9b400aaea
Philip Lewis <philip.lewis71239@gmail.com>
2015-01-07 12:22:10 -0500

    heroku database almost done

--- app.py
+++ app.py
@@ -2,34 +2,34 @@ from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+# db = SQLAlchemy(app)
 
-class Logger(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	ip_address = db.Column(db.String(400))
-	timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
+# class Logger(db.Model):
+# 	id = db.Column(db.Integer, primary_key=True)
+# 	ip_address = db.Column(db.String(400))
+# 	timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
 
-	def __init__(self,ip_address):
-		self.ip_address = ip_address
+# 	def __init__(self,ip_address):
+# 		self.ip_address = ip_address
 
-	def __repr__(self):
-		return '<ip_addr %r>' % self.ip_address
+# 	def __repr__(self):
+# 		return '<ip_addr %r>' % self.ip_address
 
 @app.route("/")
 def gateway():
 	ip = request.remote_addr
-	log = Logger(ip)
-	db.session.add(log)
-	db.session.commit()
+	# log = Logger(ip)
+	# db.session.add(log)
+	# db.session.commit()
 	return render_template("gateway.html")
 
 @app.route("/index")
 def index():
 	ip = request.remote_addr
-	log = Logger(ip)
-	db.session.add(log)
-	db.session.commit()
+	# log = Logger(ip)
+	# db.session.add(log)
+	# db.session.commit()
 	return render_template("index.html")
 
 # @app.route("/view_logs/<password>")
