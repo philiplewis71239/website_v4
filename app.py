ef1b05b1a2c6fa7738a3db67e91e1573c7116702
Philip Lewis <philip.lewis71239@gmail.com>
2015-01-07 12:13:32 -0500

    change password soon

--- app.py
+++ app.py
@@ -5,16 +5,38 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
+class Logger(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	ip_address = db.Column(db.String(400))
+	timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
+
+	def __init__(self,ip_address):
+		self.ip_address = ip_address
+
+	def __repr__(self):
+		return '<ip_addr %r>' % self.ip_address
+
 @app.route("/")
 def gateway():
 	ip = request.remote_addr
+	log = Logger(ip)
+	db.session.add(log)
+	db.session.commit()
 	return render_template("gateway.html")
 
 @app.route("/index")
 def index():
 	ip = request.remote_addr
-
+	log = Logger(ip)
+	db.session.add(log)
+	db.session.commit()
 	return render_template("index.html")
 
+@app.route("/view_logs/<password>")
+def view_logs(password):
+	if password == "temporary_password":
+		return render_template("viewer.html",results=Logger.query.all())
+	else:
+		return "Oh hi there"
 if __name__ == '__main__':
 	app.run(debug=True)
