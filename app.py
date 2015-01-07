fda6849129db1d1dd1dd45a36c0ef7579000bc48
Philip Lewis <philip.lewis71239@gmail.com>
2015-01-07 12:39:52 -0500

    annoying

--- app.py
+++ app.py
@@ -28,17 +28,17 @@ def gateway():
 @app.route("/index")
 def index():
 	ip = request.remote_addr
-	# log = Logger(ip)
-	# db.session.add(log)
-	# db.session.commit()
+	log = Logger(ip)
+	db.session.add(log)
+	db.session.commit()
 	return render_template("index.html")
 
-# @app.route("/view_logs/<password>")
-# def view_logs(password):
-# 	if password == "temporary_password":
-# 		return render_template("viewer.html",results=Logger.query.all())
-# 	else:
-# 		return "Oh hi there"
+@app.route("/view_logs/<password>")
+def view_logs(password):
+	if password == "temporary_password":
+		return render_template("viewer.html",results=Logger.query.all())
+	else:
+		return "Oh hi there"
 
 
 if __name__ == '__main__':
